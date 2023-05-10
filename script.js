a73fd997006c35ce3469af4283ce41a57f15d382
Amber Kimbrough <akimbrough14@gmail.com>
2023-05-10 16:04:33 -0500

    first attemp at GET request for data

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
 console.log("Running Scripts")
 
-var APIKey = "I7rLhP53t0wjXc4eUizXjC5LYzglaCGm"
-var NFT
 
-fetch(APIKey)
-.then(response => response.json())
-.then(data => console.log(data))
+const options = {
+    method: 'GET',
+    headers: {accept: 'application/json', 'X-API-KEY': 'I7rLhP53t0wjXc4eUizXjC5LYzglaCGm'}
+  };
+
+  const renderCards = (dataArray) => {
+    console.log(dataArray)
+    dataArray.forEach(cardInfo => {
+        console.log(cardInfo.id)
+    })
+  }
+  
+  fetch('https://api.blockspan.com/v1/nfts?chain=eth-main', options)
+    .then(response => response.json())
+    .then(response => console.log(response))
+    .catch(err => renderCards(err.id));
+
