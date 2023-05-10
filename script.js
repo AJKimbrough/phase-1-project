4a9b88c029edcdf87ac6c095eb70ef8f5973e74b
Amber Kimbrough <akimbrough14@gmail.com>
2023-05-10 18:19:49 -0500

    creates renderCards function

--- script.js
+++ script.js
@@ -1,20 +1,32 @@
 console.log("Running Scripts")
 
-
+/*
 const options = {
     method: 'GET',
     headers: {accept: 'application/json', 'X-API-KEY': 'I7rLhP53t0wjXc4eUizXjC5LYzglaCGm'}
   };
+*/
 
   const renderCards = (dataArray) => {
-    console.log(dataArray)
+    //console.log(dataArray)
     dataArray.forEach(cardInfo => {
-        console.log(cardInfo.id)
+        //for(let i = 0; i < dataArray.length; i++)
+        console.log(cardInfo.cached_images)
     })
   }
-  
+  /*
   fetch('https://api.blockspan.com/v1/nfts?chain=eth-main', options)
     .then(response => response.json())
-    .then(response => console.log(response))
-    .catch(err => renderCards(err.id));
+    .then(response => console.log(response.results))
+    .catch(err => console.log(err));
+    */
 
+    const options = {
+        method: 'GET',
+        headers: {accept: 'application/json', 'X-API-KEY': 'I7rLhP53t0wjXc4eUizXjC5LYzglaCGm'}
+      };
+      
+      fetch('https://api.blockspan.com/v1/nfts/contract/0x41f56b000fffe17943fb4c182c123767af71d005?chain=eth-main', options)
+        .then(response => response.json())
+        .then(response => renderCards(response.results))
+        .catch(err => console.error(err));
