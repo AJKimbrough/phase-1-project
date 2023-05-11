6020280a3359a886709e3f10aebfe7834174474c
Amber Kimbrough <akimbrough14@gmail.com>
2023-05-11 13:31:49 -0500

    successfull render of imgs

--- script.js
+++ script.js
@@ -16,11 +16,14 @@ const options = {
 
   const renderCards = (dataArray) => {
     //console.log(dataArray)
+    const cardContainer = document.getElementById("card-container")
     dataArray.forEach(cardInfo => {
         //for(let i = 0; i < dataArray.length; i++)
         //console.log(cardInfo.cached_images)
         const img = document.createElement("img")
-        img.src = cardInfo.cached_images
+        img.src = cardInfo.cached_images.small_250_250
+        
+        cardContainer.appendChild(img)
         
     })
   }
@@ -31,8 +34,20 @@ const options = {
         headers: {accept: 'application/json', 'X-API-KEY': 'I7rLhP53t0wjXc4eUizXjC5LYzglaCGm'}
       };
       
-      fetch('https://api.blockspan.com/v1/nfts/contract/0x41f56b000fffe17943fb4c182c123767af71d005?chain=eth-main', options)
+      fetch('https://api.blockspan.com/v1/nfts/contract/0x41f56b000fffe17943fb4c182c123767af71d005', options)
         
         .then(response => response.json())
         .then(response => renderCards(response.results))
         .catch(err => console.error(err));
+
+/*
+const options = {
+    method: 'GET',
+    headers: {accept: 'application/json', 'X-API-KEY': 'I7rLhP53t0wjXc4eUizXjC5LYzglaCGm'}
+  };
+  
+  fetch('https://api.blockspan.com/v1/nfts/contract/0xbd1b904e981af7bf7c0bf5cbaea90e1fbccd999c?chain=eth-main', options)
+    .then(response => response.json())
+    .then(response => renderCards(response.results))
+    .catch(err => console.error(err));
+    */
