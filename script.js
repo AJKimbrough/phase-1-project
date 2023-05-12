4b7bc3019f12ba64896c88449b925cdd907311d4
Amber Kimbrough <akimbrough14@gmail.com>
2023-05-11 21:06:39 -0500

    first attempt at fetch param

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 
-    
+let input;
+
+const init = () => {
+    const inputForm = document.querySelector("form")
+
+    inputForm.addEventListener("submit", (event) => {
+        event.preventDefault()
+        input = document.querySelector("input#searchByAddress")
+        console.log(input.value)
+    })
+}
+
+
+
+
   const renderCards = (dataArray) => {
    
     const cardContainer = document.getElementById("card-container")
@@ -7,25 +21,22 @@
 
         const img = document.createElement("img")
         img.src = cardInfo.cached_images.small_250_250
-        const btn = document.createElement("btn")
     
         cardContainer.appendChild(img)
 
-        
-        
-
     })
   }
-  
 
     const options = {
         method: 'GET',
         headers: {accept: 'application/json', 'X-API-KEY': 'I7rLhP53t0wjXc4eUizXjC5LYzglaCGm'}
       };
       
-      fetch('https://api.blockspan.com/v1/nfts/contract/0x41f56b000fffe17943fb4c182c123767af71d005', options)
+      fetch(`https://api.blockspan.com/v1/nfts/contract/${input.value}`, options)
         
         .then(response => response.json())
         .then(response => renderCards(response.results))
         .catch(err => console.error(err));
 
+    
+        document.addEventListener("DOMContentLoaded", init)
