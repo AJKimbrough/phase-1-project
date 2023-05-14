33cebd069220fae61cf07031b19413987f06eac7
Amber Kimbrough <akimbrough14@gmail.com>
2023-05-14 17:42:53 -0500

    fixed navigation links

--- script.js
+++ script.js
@@ -1,97 +1,90 @@
+const modal = document.querySelector(".modal");
+
+const bootstrapModal = new bootstrap.Modal(modal, {
+  keyboard: true,
+  backdrop: true,
+  focus: true,
+});
+
+const renderCards = (cardDataArray) => {
+  const cardContainer = document.getElementById("card-container");
+  
+  cardDataArray.forEach((cardInfo) => {
+    const img = document.createElement("img");
+    img.src = cardInfo.img;
+    img.className = "cardInfo-image";
+    img.id = cardInfo.id;
+    img.dataset.toggle = "modal";
+    img.dataset.target = `cardInfo-${cardInfo.id}`;
+    cardContainer.appendChild(img);
+    img.addEventListener("dblclick", () => renderModal(cardInfo));
+  });
+};
 
 
-const renderCardsHome = (dataArray) => {
-  const cardContainer = document.getElementById("card-container")
-  dataArray.forEach(cardInfo => {
-    const img = document.createElement("img")
-    const imgContainer = document.createElement("div")
-    //img.src = cardInfo.cached_images.small_250_250
-    img.src = cardInfo.img
-    //console.log(cardInfo)
 
-    img.addEventListener("click", (event) => {
-      //console.log(event.target)
-      const pName = document.createElement("p")
-      pName.textContent = cardInfo.name
-      cardContainer.append(pName)
-      console.log(pName)
+const populateModalData = (data, modal) => {
+  const name = modal.querySelector(".modal-name");
+  const description = modal.querySelector(".modal-description");
+  const modalImg = modal.querySelector(".modal-img");
+  const modalPrice = modal.querySelector(".modal-price");
 
-    })
+  name.textContent = data.name;
+  description.textContent = data.description;
+  modalImg.src = data.img;
+  modalPrice.textContent = data.price
+  
 
-    
-    cardContainer.appendChild(img)
+  if (data.drops) {
+    modalDropsHeading.style.display = "inline-block"
+    data.drops.forEach((item) => {
+      const li = document.createElement("li");
+      li.textContent = item;
+      modalDrops.appendChild(li);
+    });
+  }
+};
 
-  })
-}
+
+const renderModal = (cardData) => {
+  populateModalData(cardData, modal);
+  bootstrapModal.show(); 
+};
+
 
 const addEventListenerToNavLinks = () => {
   const links = document.querySelectorAll(".link")
   links.forEach(link => {
     link.addEventListener("click", (e) => {
-      console.log(e.target)
-      fetch('http://localhost:3000/NFTs')
-      .then(res => res.json())
-      .then(info => renderCardsHome(info))
+      fetch(`http://localhost:3000/Best`)
+      .then(response => response.json())
+      .then(info => {
+          console.log(info)
+          renderCards(info)
+          return info
+      })
       })
     })
-  
 }
 
-  const renderCards = (dataArray) => {
-   
-    const cardContainer = document.getElementById("card-container")
-    dataArray.forEach(cardInfo => {
-
-        const img = document.createElement("img")
-      
-        img.src = cardInfo.cached_images.small_250_250
-        img.addEventListener("dblclick", (event) => {
-            console.log(cardInfo.recent_price.price)
-            const pName = document.createElement("p")
-            pName.textContent = cardInfo.token_name
-            console.log(pName)
-        })
-
-        cardContainer.appendChild(img)
-
-    })
-  }
-
-  
-  const fetchData = () => {
-    const inputForm = document.querySelector("form")
-
-    inputForm.addEventListener("submit", (event) => {
-        event.preventDefault()
-        const input = document.querySelector("input#searchByAddress")
-        
-
-      const options = {
-        method: 'GET',
-        headers: {accept: 'application/json', 'X-API-KEY': 'I7rLhP53t0wjXc4eUizXjC5LYzglaCGm'}
-      };
-      
-      fetch(`https://api.blockspan.com/v1/nfts/contract/${input.value}`, options)
-        
-        .then(response => response.json())
-        .then(response => renderCards(response.results))
-        .catch(err => console.error(err));
-       
-    })
-}
+  const navLinks = document.querySelectorAll(".nav-link")
+    navLinks.forEach((link) => link.addEventListener("click", handleNavClick))
 
 
-/*
-   fetchData()
-   //initData()
-   //renderNFTData()
-*/
 const fetchHomeData = () => {
   return fetch('http://localhost:3000/NFTs')
   .then(res => res.json())
-  .then(info => renderCardsHome(info))
+  .then(info => renderCards(info))
   .catch(err => console.error(err))
 }
 
+/*
+document.addEventListener("DOMContentLoaded", () => {
+  Object.keys(NFTs).forEach(key => {
+    fetchHomeData(key).then(({data}) => saveData(flatten(data), key)).then(data => renderCardsHome(flatten(NFTs)))
+  })
+})
+*/
 fetchHomeData()
 addEventListenerToNavLinks()
+//fetchData()
