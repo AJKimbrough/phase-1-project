8d6ff3fef8163ae3bca7263453d40af0ea815be2
Amber Kimbrough <akimbrough14@gmail.com>
2023-05-14 10:53:23 -0500

    add renderCardsTwo

--- script.js
+++ script.js
@@ -6,7 +6,7 @@
 
         const img = document.createElement("img")
         
-        img.src = cardInfo.cached_images.tiny_100_100
+        img.src = cardInfo.cached_images.small_250_250
         img.addEventListener("dblclick", (event) => {
             console.log(cardInfo.recent_price.price)
             const pName = document.createElement("p")
@@ -19,6 +19,7 @@
     })
   }
 
+  
   const init = () => {
     const inputForm = document.querySelector("form")
 
@@ -42,11 +43,31 @@
 }
 
 
-
-
-
+const renderCardsTwo = (dataArray) => {
+  const cardContainer = document.getElementById("card-container")
+  dataArray.forEach(cardInfo => {
+    const img = document.createElement("img")
+    img.src = cardInfo.cached_images.small_250_250
 
     
+    cardContainer.appendChild(img)
+
+  })
+}
+
+const options = {
+  method: 'GET',
+  headers: {accept: 'application/json', 'X-API-KEY': 'I7rLhP53t0wjXc4eUizXjC5LYzglaCGm'}
+};
+
+fetch('https://api.blockspan.com/v1/nfts/contract/0x41f56b000fffe17943fb4c182c123767af71d005', options)
+  
+  .then(response => response.json())
+  .then(response => renderCardsTwo(response.results))
+  .catch(err => console.error(err));
+ 
+
+
 
    init()
    //initData()
